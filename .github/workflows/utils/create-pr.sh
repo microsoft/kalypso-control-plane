5f6987702c37f0262dec5d3433378bd7bb346df3
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-12-14 10:37:38 -0800

    pr

--- .github/workflows/utils/create-pr.sh
+++ .github/workflows/utils/create-pr.sh
@@ -57,7 +57,7 @@ git checkout -b $deploy_branch_name
 
 # Add generated manifests to the new deploy branch
 mkdir -p $DEST_FOLDER
-cp -r $SOURCE_FOLDER/* $DEST_FOLDER/
+cp -r $SOURCE_FOLDER/. $DEST_FOLDER/
 git add -A
 echo "git status"
 git status
