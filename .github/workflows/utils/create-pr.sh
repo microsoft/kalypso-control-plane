526e169ed54ac0e3c1309241ce5aeb2610b3be8f
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-12-14 16:49:38 -0800

    transform

--- .github/workflows/utils/create-pr.sh
+++ .github/workflows/utils/create-pr.sh
@@ -78,7 +78,7 @@ if [[ `git status --porcelain | head -1` ]]; then
     owner_repo="${DEST_REPO#https://github.com/}"
     echo $owner_repo
     export GITHUB_TOKEN=$TOKEN
-    pr_response=$(gh pr create --repo $repo_url --base $DEST_BRANCH --head $deploy_branch_name --title "deployment $DEPLOY_ID" --body "Deploy to $ENV_NAME" --label $LABEL)
+    pr_response=$(gh pr create --repo $repo_url --base $DEST_BRANCH --head $deploy_branch_name --title "deployment $DEPLOY_ID" --body "Deploy to $ENV_NAME")
     echo $pr_response
     pr_num="${pr_response##*pull/}"
     echo $pr_num
