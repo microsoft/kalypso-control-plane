82c703dfa4cd650d24c32be41fa0a1ed07a00dd8
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-12-20 15:10:42 -0800

    token

--- .github/workflows/utils/transform.sh
+++ .github/workflows/utils/transform.sh
@@ -18,7 +18,7 @@ WORKLOAD_REPO=$(yq '.spec.workload.repo' $WORKLOAD)
 WORKLOAD_BRANCH=$(yq '.spec.workload.branch' $WORKLOAD)
 WORKLOAD_PATH=$(yq '.spec.workload.path' $WORKLOAD)
 
-gh auth login --with-token $TOKEN
+gh auth login --with-token <<<"$TOKEN"
 git clone $WORKLOAD_REPO --depth 1 --branch $WORKLOAD_BRANCH workload
 
 export DEPLOYMENT_TARGET_REPO=$(yq '.spec.deploymentTargets[0].manifests.repo' workload/$WORKLOAD_PATH)
