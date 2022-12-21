9f0e0097d69d0e830cdce6351a4fb3d0ee1ec2b4
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-12-20 16:01:25 -0800

    wtf

--- .github/workflows/utils/transform.sh
+++ .github/workflows/utils/transform.sh
@@ -10,32 +10,32 @@
 
 echo "before something"
 
-WORKLOAD=$1
-TEMPLATE=$2
-echo "something"
-echo $WORKLOAD
-echo $TEMPLATE
-pwd
+# WORKLOAD=$1
+# TEMPLATE=$2
+# echo "something"
+# echo $WORKLOAD
+# echo $TEMPLATE
+# pwd
 
-WORKLOAD_NAME=$(yq '.metadata.name' $WORKLOAD)
-WORKLOAD_WORKSPACE=$(yq '.spec.workspace' $WORKLOAD)
+# WORKLOAD_NAME=$(yq '.metadata.name' $WORKLOAD)
+# WORKLOAD_WORKSPACE=$(yq '.spec.workspace' $WORKLOAD)
 
-WORKLOAD_REPO=$(yq '.spec.workload.repo' $WORKLOAD)
-WORKLOAD_BRANCH=$(yq '.spec.workload.branch' $WORKLOAD)
-WORKLOAD_PATH=$(yq '.spec.workload.path' $WORKLOAD)
+# WORKLOAD_REPO=$(yq '.spec.workload.repo' $WORKLOAD)
+# WORKLOAD_BRANCH=$(yq '.spec.workload.branch' $WORKLOAD)
+# WORKLOAD_PATH=$(yq '.spec.workload.path' $WORKLOAD)
 
-echo $WORKLOAD_REPO
-gh auth login --with-token <<<"$TOKEN"
-# git clone $WORKLOAD_REPO --depth 1 --branch $WORKLOAD_BRANCH workload
+# echo $WORKLOAD_REPO
+# gh auth login --with-token <<<"$TOKEN"
+# # git clone $WORKLOAD_REPO --depth 1 --branch $WORKLOAD_BRANCH workload
 
-export DEPLOYMENT_TARGET_REPO=$(yq '.spec.deploymentTargets[0].manifests.repo' workload/$WORKLOAD_PATH)
-export DEPLOYMENT_TARGET_BRANCH=$(yq '.spec.deploymentTargets[0].manifests.branch' workload/$WORKLOAD_PATH)
-export DEPLOYMENT_TARGET_PATH=$(yq '.spec.deploymentTargets[0].manifests.path' workload/$WORKLOAD_PATH)
+# export DEPLOYMENT_TARGET_REPO=$(yq '.spec.deploymentTargets[0].manifests.repo' workload/$WORKLOAD_PATH)
+# export DEPLOYMENT_TARGET_BRANCH=$(yq '.spec.deploymentTargets[0].manifests.branch' workload/$WORKLOAD_PATH)
+# export DEPLOYMENT_TARGET_PATH=$(yq '.spec.deploymentTargets[0].manifests.path' workload/$WORKLOAD_PATH)
 
-export DEPLOYMENT_TARGET_NAME=$(yq '.spec.deploymentTargets[0].name' workload/$WORKLOAD_PATH)
-export DEPLOYMENT_TARGET_ENVIRONMENT=$(yq '.spec.deploymentTargets[0].environment' workload/$WORKLOAD_PATH)
-export DEPLOYMENT_TARGET_NAMESPACE=$DEPLOYMENT_TARGET_ENVIRONMENT-$WORKLOAD_WORKSPACE-$WORKLOAD_NAME-$DEPLOYMENT_TARGET_NAME
+# export DEPLOYMENT_TARGET_NAME=$(yq '.spec.deploymentTargets[0].name' workload/$WORKLOAD_PATH)
+# export DEPLOYMENT_TARGET_ENVIRONMENT=$(yq '.spec.deploymentTargets[0].environment' workload/$WORKLOAD_PATH)
+# export DEPLOYMENT_TARGET_NAMESPACE=$DEPLOYMENT_TARGET_ENVIRONMENT-$WORKLOAD_WORKSPACE-$WORKLOAD_NAME-$DEPLOYMENT_TARGET_NAME
 
-echo $DEPLOYMENT_TARGET_NAMESPACE
+# echo $DEPLOYMENT_TARGET_NAMESPACE
 
-cat $TEMPLATE | sed -r 's/[{{]+/$/g' | sed 's/}}//g' | envsubst
+# cat $TEMPLATE | sed -r 's/[{{]+/$/g' | sed 's/}}//g' | envsubst
