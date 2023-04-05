009e613d4dbea36870cf7b134298ec502617f6ea
Eugene Fedorenko <eugene.fedor@gmail.com>
2023-04-04 17:24:08 -0700

    setup

--- .onboard/ubuntu/onboard.sh
+++ .onboard/ubuntu/onboard.sh
@@ -12,6 +12,19 @@ CONTROL_PLANE_URL=$1 # https://github.com/eedorenko/kalypso-oci-gitops
 CONTROL_PLANE_BRANCH=$2 # dev
 CONTROL_PLANE_FOLDER=$3 # vm
 
+print_usage() {
+    printf "Usage: ./opt/kalypso/onboard.sh <gitops repo> <branch> <folder> \n"
+    printf "\nExample ./opt/kalypso/onboard.sh https://github.com/eedorenko/kalypso-oci-gitops dev vm \n"                                                              
+    exit 1
+}
+
+
+if [ -z $CONTROL_PLANE_URL ] || [ -z $CONTROL_PLANE_BRANCH ] || [ -z $CONTROL_PLANE_FOLDER ];
+then
+ print_usage
+fi
+
+
 ansible-playbook -b /opt/kalypso/onboard.yml -e "repo_url=$CONTROL_PLANE_URL" -e "repo_branch=$CONTROL_PLANE_BRANCH" -e "repo_folder=$CONTROL_PLANE_FOLDER"
 
 
--- .onboard/ubuntu/setup.sh
+++ .onboard/ubuntu/setup.sh
@@ -21,33 +21,12 @@ print_prerequisites() {
 
 check_prerequisites || print_prerequisites
 
-mkdir -p /opt/kalypso1/log
+kalypso_home=/opt/kalypso1
 
-curl -fsSL -o /opt/kalypso1/ https://raw.githubusercontent.com/microsoft/kalypso-control-plane/eedorenko/ansible/.onboard/ubutu/
+mkdir -p $kalypso_home/log
 
-## Install ansible
-## copy files
+curl -fsSL -o $kalypso_home/onboard.sh https://raw.githubusercontent.com/microsoft/kalypso-control-plane/eedorenko/ansible/.onboard/ubuntu/onboard.sh
+curl -fsSL -o $kalypso_home/install-deb-package.yml https://raw.githubusercontent.com/microsoft/kalypso-control-plane/eedorenko/ansible/.onboard/ubuntu/install-deb-package.yml
+curl -fsSL -o $kalypso_home/onboard.yml https://raw.githubusercontent.com/microsoft/kalypso-control-plane/eedorenko/ansible/.onboard/ubuntu/onboard.yml
+curl -fsSL -o $kalypso_home/reconcile.sh https://raw.githubusercontent.com/microsoft/kalypso-control-plane/eedorenko/ansible/.onboard/ubuntu/reconcile.sh
 
-# sudo apt-add-repository -y ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get install -y ansible
-
-# install yq
-
-# upddate /etc/ansible/hosts with
-
-# mkdir -p /opt/kalypso/log
-
-# [local]
-# 127.0.0.1
-
-# [all:vars]
-# ansible_user=test
-# ansible_ssh_pass=test
-
-# ansible all -m ping
-
-# ansible-pull -o -U https://github.com/eedorenko/kalypso-oci-gitops -C dev -d vm | logger -p local4.debug
-
-#         # ./opt/kalypso/reconcile.sh {{repo_url}} {{repo_branch}} {{repo_folder}}  >> /opt/kalypso/log/gitops-{{repo_branch}}-{{repo_folder}}.log 2>>&1
-#         job: "./opt/kalypso/reconcile.sh {{repo_url}} {{repo_branch}} {{repo_folder}} > /opt/kalypso/log/gitops-{{repo_branch}}-{{repo_folder}}.log 2>&1"
